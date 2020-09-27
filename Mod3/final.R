fbdb1df5257b0467cc854e3f2315e4ad38fd8241
yeokunsong <yeokunsong@gmail.com>
2020-09-27 11:39:12 +0800

    completed final

--- Mod3/final.R
+++ Mod3/final.R
@@ -9,20 +9,23 @@ train_x <- read.table("Mod3/UCI HAR Dataset/train/X_train.txt")
 train_y <- read.table("Mod3/UCI HAR Dataset/train/y_train.txt")
 test_x <- read.table("Mod3/UCI HAR Dataset/test/X_test.txt")
 test_y <- read.table("Mod3/UCI HAR Dataset/test/y_test.txt")
+subject_train <- read.table("Mod3/UCI HAR Dataset/train/subject_train.txt")
+subject_test <- read.table("Mod3/UCI HAR Dataset/test/subject_test.txt")
 
 # 1. Merges the training and the test sets to create one data set.
 
 X <- rbind(train_x,test_x)
 Y <- rbind(train_y,test_y)
-df <- cbind(X,Y)
-names(df) <- c(dfcolnames,"Activity")
+subject <- rbind(subject_train, subject_test)
+df <- cbind(subject,X,Y)
+names(df) <- c("Subject",dfcolnames,"Activity")
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 
 library(dplyr)
 #grep("(mean|std)\\(\\)",dfcolnames, value = 1) 
 dfextract <- df %>% 
-  select((contains("mean()") | contains("std()")), "Activity")
+  select("Subject",(contains("mean()") | contains("std()")), "Activity",)
 
 # 3. Uses descriptive activity names to name the activities in the data set
 
@@ -35,8 +38,15 @@ dfextract <- dfextract %>%
   mutate (Activity = NULL)
 
 # 4. Appropriately labels the data set with descriptive variable names.
-
+names <- names(dfextract)
+names <- gsub("Acc","Acceleration",names)
+names <- gsub("Mag","Magnitude",names)
 
 # 5. From the data set in step 4, creates a second, independent tidy data 
 #set with the average of each variable for each activity and each subject.
-        
+dffinal <- dfextract %>%
+  group_by(Subject,Activitydesc) %>% 
+  summarise_all(mean)
+names(dffinal)[3:ncol(dffinal)] <- sapply(names(dffinal)[3:ncol(dffinal)], function(x) {paste0("Mean of ",x)}) 
+
+       
