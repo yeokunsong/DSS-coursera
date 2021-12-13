8be77e83759b26d42a9ec948d826382f360b907d
KS RS Desktop <yeokunsong@gmail.com>
2021-12-13 14:24:55 +0800

    New

--- Mod5/week2(admin).R
+++ Mod5/week2(admin).R
@@ -4,3 +4,7 @@ URL = "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip"
 temp = tempfile()
 download.file(URL,destfile = temp)
 unzip(temp,"activity.csv")
+
+
+library(knitr)
+knit2html("week2.Rmd")
